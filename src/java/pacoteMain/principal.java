52983e552f8fd5887b14cda992d7fa5aadb8ab74
saovicto <victor.campos.depaula@gmail.com>
2019-04-11 20:23:19 -0300

    tentativar de adicionar a main

---
+++ src/java/pacoteMain/principal.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacoteMain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import modelo.Categoria;
+import modelo.Cliente;
+import modelo.Pedido;
+import modelo.Produto;
+import modelo.ProdutoExportacao;
+import modelo.ProdutoMercadoInterno;
+
+/**
+ *
+ * @author mateu
+ */
+
+public class principal {
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PrAula1610PU");
+        EntityManager em = emf.createEntityManager();
+        
+        for(int i=0; i<3; i++)
+        {
+            Categoria c1 = new Categoria();
+            Cliente cl1 = new Cliente();
+            Pedido p = new Pedido();
+            ProdutoExportacao pe = new ProdutoExportacao();
+            ProdutoMercadoInterno pmi = new ProdutoMercadoInterno();
+            Produto pr = new Produto();
+            
+            c1.setDescricao("Descricao" + i + "");
+            
+            cl1.setNome("Nome" + i + "");
+            
+            p.setCliente(cl1);
+            
+           
+            pe.setNome("Nome" + i + "");
+            pe.setDestino("Destino" + i + "");
+            
+            pmi.setNome("Nome" + i + "");
+            pmi.setIncentivo(true);
+            
+            pr.setNome("Nome" + i + "");
+            
+            em.getTransaction().begin();
+                em.persist(pe);
+                em.persist(pmi);
+                em.persist(pr);
+                em.persist(c1);
+                em.persist(cl1);
+                em.persist(p);
+            em.getTransaction().commit();
+        }
+            em.close();
+    }
+}
