56ce9a3c090cfd6c826ce0c9b4b18f74232cb88d
Victor Campos <victor.campos.depaula@gmail.com>
2019-06-11 23:35:24 -0300

    dao

--- src/java/servico/DAO.java
+++ src/java/servico/DAO.java
@@ -11,10 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
-<<<<<<< HEAD
-import modelo.Cliente;
-=======
->>>>>>> origin/devtest
+
 
 /**
  * 
